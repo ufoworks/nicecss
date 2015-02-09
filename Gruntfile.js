a872144ba58e43d51bd93d2b2527d3cd559f0735
Oscar <oscar@nicemondays.com>
2015-02-09 22:38:13 +0100

    Added init comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,11 +24,20 @@ module.exports = function(grunt) {
           'test/css/main.css':'nicecss.scss'
         }
       }
+    },
+    sassdoc: {
+      default: {
+        src: ['*.scss','**!(bower_components)/*.scss'],
+        options: {
+          dest: 'docs'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sassdoc');
   // Default task(s).
   grunt.registerTask('default', ['watch']);
   grunt.registerTask('dev', ['sass']);
